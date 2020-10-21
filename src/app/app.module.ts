1c6b67733ef534ba1a52d7027886caf156d5cca0
MarioM <mariiomagdy@gmail.com>
2020-10-21 14:23:13 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './content/main/main.component';
+import { ListComponent } from './content/list/list.component';
+import { ItemComponent } from './content/item/item.component';
+import { RoutesComponent } from './content/routes/routes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListComponent,
+    ItemComponent,
+    RoutesComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
